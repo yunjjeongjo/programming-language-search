b3bb8bb0b9e9efdfb041a48c04e38633041149d3
yunjjeongjo <two6885@gmail.com>
2022-08-29 17:12:41 +0900

    feat: input에 자동 focus 추가

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -2,11 +2,14 @@ export default function SearchForm({ $target, initialState, onChange }) {
   this.$element = document.createElement("form");
   this.$element.className = "SearchInput";
   this.state = initialState;
+
   $target.appendChild(this.$element);
 
   this.render = () => {
     this.$element.innerHTML = `
         <input class="SearchInput__input" type="text" placeholder="프로그램 언어를 입력하세요.">`;
+    this.$li = document.querySelector("input");
+    this.$li.focus();
     this.$element.addEventListener("keyup", (e) => {
       const actionIgnoreKeys = [
         "Enter",
